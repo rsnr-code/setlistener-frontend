ee75ca07803b57d61824a4b48447bded2a0c765b
rsnr-code <rishadnero@gmail.com>
2022-10-26 17:31:50 -0400

    added heroku post build script

--- set-listener/server.js
+++ set-listener/server.js
@@ -11,6 +11,12 @@ const app = express();
 app.use(cors());
 
 
+app.use(express.static(path.join(__dirname, 'build')));
+    
+app.get('/', function (req, res) {
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
+
 app.get('/setlist', (req, res) => {
 const artist = req.query.artistName
 
